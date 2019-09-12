97128791217c42b171547f3cd3ef77c276c90d87
Yuri WIllians dos Santos <42418401+ShieldOfDead@users.noreply.github.com>
2019-09-12 13:45:20 -0400

    Add files via upload

---
+++ src/model/bean/Registro.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Yuri Willians
+ */
+public class Registro {
+    
+    private String nome;
+    private String matricula;
+    private String horario;
+    private String computador;
+    private String local_padrao;
+
+    public String getLocal_padrao() {
+        return local_padrao;
+    }
+
+    public void setLocal_padrao(String local_padrao) {
+        this.local_padrao = local_padrao;
+    }
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public String getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return the horario
+     */
+    public String getHorario() {
+        return horario;
+    }
+
+    /**
+     * @param horario the horario to set
+     */
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    /**
+     * @return the computador
+     */
+    public String getComputador() {
+        return computador;
+    }
+
+    /**
+     * @param computador the computador to set
+     */
+    public void setComputador(String computador) {
+        this.computador = computador;
+    }
+    
+    @Override
+    public String toString() {
+        return getMatricula();
+    }
+    
+}
